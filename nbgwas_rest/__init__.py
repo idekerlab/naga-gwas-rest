93bfbf1d77adf908915a72803c88a07451723e4c
Chris Churas <churas.camera@gmail.com>
2018-11-16 13:38:21 -0800

    added task based implementation. not working yet

--- nbgwas_rest/__init__.py
+++ nbgwas_rest/__init__.py
@@ -6,16 +6,15 @@ __author__ = """Chris Churas"""
 __email__ = 'churas.camera@gmail.com'
 __version__ = '0.1.0'
 
-from nbgwas import Nbgwas
-from flask import Flask, request, jsonify
-from flask_restplus import reqparse, abort, Api, Resource
-import pandas as pd
-import networkx as nx
-import logging
+import os
+import shutil
 import json
-from ndex2 import create_nice_cx_from_server
+import uuid
+import time
+import flask
+from flask import Flask, request, jsonify, Response
+from flask_restplus import reqparse, abort, Api, Resource, fields
 
-from nbgwas_rest import bigim
 desc = """A REST service for an accessible, fast and customizable network propagation system 
 for pathway interpretation of Genome Wide Association Studies (GWAS)
 """
@@ -23,115 +22,189 @@ for pathway interpretation of Genome Wide Association Studies (GWAS)
 app = Flask(__name__, instance_relative_config=True)
 api = Api(app, version=str(__version__),
           title='Network Boosted Genome Wide Association Studies (NBGWAS) ',
-          description=desc)
+          description=desc, example='put example here')
 app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
 
+ALPHA_PARAM = 'alpha'
+NETWORK_PARAM = 'network'
+COLUMN_PARAM = 'column'
+SEEDS_PARAM = 'seeds'
+NDEX_PARAM = 'ndex'
 
-def create_gene_level_summary(genes, seeds):
-    # Add heat to gene_level_summary
+JOB_PATH = '/tmp/nbgwas'
+TASK_JSON = 'task.json'
+NETWORK_DATA = 'network.data'
+LOCATION = 'Location'
+RESULT = 'result.json'
+WAIT_COUNT = 60
+SLEEP_TIME = 10
 
-    genes_level_summary = pd.DataFrame([genes], index=['Genes']).T
-    genes_level_summary['p-value'] = 1
-    genes_level_summary.loc[genes_level_summary['Genes'].isin(seeds), 'p-value'] = 0
 
-    return genes_level_summary
+def create_task(request_obj):
+    params = {}
+    params[ALPHA_PARAM] = float(request_obj.values.get(ALPHA_PARAM, 0.5))
+
+    if SEEDS_PARAM in request_obj.values:
+        params[SEEDS_PARAM] = request_obj.values[SEEDS_PARAM]
+
+    params['uuid'] = str(uuid.uuid4())
+
+    taskpath = os.path.join(JOB_PATH, request_obj.remote_addr, params['uuid'])
+    os.makedirs(taskpath, mode=0o755)
+
+    # Getting network
+    if NETWORK_PARAM in request_obj.files:
+        network_file = request_obj.files[NETWORK_PARAM]
+        app.logger.debug('Networkfile: ' + str(network_file))
+        networkfile_path = os.path.join(taskpath, NETWORK_DATA)
+
+        with open(networkfile_path, 'wb') as f:
+            shutil.copyfileobj(network_file.stream, f)
+            f.flush()
+    elif COLUMN_PARAM in request_obj.values:
+        app.logger.debug("Getting file from BigGIM")
+        params[COLUMN_PARAM] = request_obj.values[COLUMN_PARAM]  # what is 0.8?
+
+    elif NDEX_PARAM in request_obj.values:
+        app.logger.debug("Getting network from NDEx")
+        params[NDEX_PARAM] = request_obj.values[NDEX_PARAM]
+    else:
+        raise Exception('One of the three parameters must be '
+                        'passed with request: ' + NETWORK_PARAM +
+                        ', ' + COLUMN_PARAM + ', ' + NDEX_PARAM)
+
+    tmp_task_json = TASK_JSON + '.tmp'
+    taskfilename = os.path.join(taskpath, tmp_task_json)
+    with open(taskfilename, 'w') as f:
+        json.dump(params, f)
+        f.flush()
+
+    os.rename(tmp_task_json, os.path.join(taskpath, TASK_JSON))
+    return params['uuid']
 
 
-ALPHA_PARAM='alpha'
-NETWORK_PARAM='network'
-COLUMN_PARAM='column'
-SEEDS_PARAM='seeds'
-NDEX_PARAM='ndex'
+def get_task(uuidstr, hintlist=None, basedir=JOB_PATH):
 
-@api.route('/nbgwas', endpoint='nbgwas')
-class nbgwasapp(Resource):
+    if hintlist is not None:
+        for subdir in hintlist:
+            taskpath = os.path.join(basedir, subdir, uuidstr)
+            if os.path.isdir(taskpath):
+                return taskpath
 
+    for entry in os.listdir(basedir):
+        fullpath = os.path.join(basedir, entry)
+        if not os.path.isdir(fullpath):
+            taskpath = os.path.join(fullpath, uuidstr)
+            if os.path.isdir(taskpath):
+                return taskpath
+    return None
+
+
+# NETWORK_PARAM: fields.Arbitrary(description='If set, loads network from file (TODO explain format)')
+task_fields = api.model('tasks', {
+    ALPHA_PARAM: fields.Float(0.2, min=0.0, max=1.0,
+                              description='Alpha parameter to use in random walk function'),
+    NDEX_PARAM: fields.String(None, description='If set, grabs network matching ID from NDEX http://http://www.ndexbio.org/'),
+    SEEDS_PARAM: fields.String(None, description='Comma list of genes...'),
+    COLUMN_PARAM: fields.String(None, description='Setting this gets network from bigim?'),
+})
+
+@api.doc('Runs NEtwork boosted gwas')
+@api.route('/nbgwas/tasks', endpoint='nbgswas/tasks')
+class TaskBasedRestApp(Resource):
 
     @api.doc('Runs Network Boosted GWAS',
-             params={ALPHA_PARAM: 'Alpha parameter to use in random walk '
-                                            'with restart model function should be set to values between 0-1',
-                     NDEX_PARAM: 'If set, grabs network matching ID from NDEX http://http://www.ndexbio.org/',
-                     NETWORK_PARAM: 'If set, loads network from file (TODO explain format)',
-                     SEEDS_PARAM: 'Comma list of genes...',
-                     COLUMN_PARAM: 'Setting this gets network from bigim?'
-
-    },
-        responses={
-        200: 'Success'
-    })
+             responses={
+                 202: 'Success',
+                 500: 'Internal server error'
+             })
+    @api.header(LOCATION, 'If successful, URL of created task', example='he')
+    @api.expect(task_fields)
     def post(self):
-        logging.info("Begin!")
-        # Setting alpha
-        alpha = float(request.values.get(ALPHA_PARAM, 0.5))
-        dG = None
-        # Getting network
-        if NETWORK_PARAM in request.files:
-            logging.info("Reading Network File")
+        """
+        Runs Network Boosted GWAS asynchronously
 
-            network_file = request.files[NETWORK_PARAM]
-            network_df = pd.read_csv(
-                network_file.stream,
-                sep='\t',
-                names=['Gene1', 'Gene2', 'Val']
-            )
-        elif COLUMN_PARAM in request.values:
-            logging.info("Getting file from BigGIM")
-            network_df = bigim.get_table_from_biggim(request.values[COLUMN_PARAM], 0.8)
-            network_df = network_df.astype(str)
+        Some more information here
+        """
+        app.logger.debug("Begin!")
 
-        elif NDEX_PARAM in request.values:
-            logging.info("Getting network from NDEx")
-            ndex_uuid = request.values[NDEX_PARAM]
-            network_niceCx = create_nice_cx_from_server(
-                server='public.ndexbio.org',
-                uuid=ndex_uuid
-            )
-            dG = network_niceCx.to_networkx()
-            node_name_mapping = {i: j['name'] for i, j in dG.node.items()}
+        try:
+            res = create_task(request)
+            resp = flask.make_response()
+            resp.headers[LOCATION] = 'nbgwas/tasks/' + res
+            resp.status_code = 202
+            return resp
+        except OSError as e:
+            app.logger.exception('Error creating task')
+            abort(500, 'Unable to create task')
 
-            dG = nx.relabel_nodes(dG, node_name_mapping)
+    def delete(self):
+        resp = flask.make_response()
+        resp.status_code = 200
+        return resp
 
-        else:
-            return "Query fields are not understood!"
 
-        logging.info("Finished getting network_df")
+@api.route('/nbgwas/tasks/<string:id>', endpoint='nbgwas/tasks')
+class TaskGetterApp(Resource):
 
-        # Making networkx object
-        if dG is None:
-            dG = nx.from_pandas_dataframe(network_df, 'Gene1', 'Gene2')
+    def head(self):
+        resp = flask.make_response()
+        resp.status_code = 200
+        return resp
 
-        logging.info("Finished making network")
+    def get(self):
+        resp = flask.make_response()
+        resp.status_code = 200
+        return resp
 
-        # Parsing seeds
-        seeds = request.values[SEEDS_PARAM]
-        seeds = seeds.split(',')
+    def delete(self):
+        resp = flask.make_response()
+        resp.status_code = 200
+        return resp
 
-        logging.info("Finished converting seeds")
 
-        # Making sure the seeds are in the network
-        for i in seeds:
-            if i not in dG.nodes():
-                seeds.remove(i)
-                logging.info(f"{i} not in nodes")
+@api.doc('Runs Network Boosted GWAS in legacy mode', example='class level examplexxxx')
+@api.route('/nbgwas', endpoint='nbgswas')
+class RestApp(Resource):
+    """Old interface that returns the result immediately"""
 
-        if len(seeds) == 0:
-            logging.info("No seeds left!")
-            return "failed"
+    @api.doc('hello', params={ALPHA_PARAM: 'Alpha parameter to use in random walk '
+                                           'with restart model function should be set to values between 0-1',
+                              NDEX_PARAM: 'If set, grabs network matching ID from NDEX http://http://www.ndexbio.org/',
+                              NETWORK_PARAM: 'If set, loads network from file (TODO explain format)',
+                              SEEDS_PARAM: 'Comma list of genes...',
+                              COLUMN_PARAM: 'Setting this gets network from bigim?'
+                              },
+             description='Legacy REST service that runs NBGWAS and waits for result',
+             example='Add example here',
+             responses={
+                 200: 'Success',
+                 500: 'Internal server error'
+             })
+    @api.deprecated
+    def post(self):
+        """Runs Network Boosted GWAS"""
+        app.logger.debug("Begin!")
 
-        gene_level_summary = create_gene_level_summary(dG.nodes(), seeds)
+        try:
+            res = create_task(request)
+            counter = 0
 
-        logging.info("Finished gene level Summary")
+            while counter < WAIT_COUNT:
+                taskpath = get_task(res)
+                if taskpath is not None:
+                    break
+                app.logger.debug('Sleeping while waiting for ' + res)
+                time.sleep(SLEEP_TIME)
 
-        g = Nbgwas(
-            gene_level_summary=gene_level_summary,
-            gene_col='Genes',
-            gene_pval_col='p-value',
-            network=dG,
-        )
+            result = os.path.join(taskpath, RESULT)
+            with open(result, 'r') as f:
+                data = json.load(result)
 
-        g.convert_to_heat()
-        g.diffuse(method='random_walk', alpha=alpha)
-
-        logging.info("Done!")
-        return_json = json.loads(g.heat.iloc[:, -1].to_json())
-        return return_json  # + '\n'
+            resp = flask.make_response()
+            resp.status_code = 200
+            resp.data = data
+            return resp
+        except OSError as e:
+            app.logger.exception('Error creating task')
+            abort(500, 'Unable to create task')
