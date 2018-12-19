779e0fc5cdca258500732fa4d089eac15643a9f5
Chris Churas <churas.camera@gmail.com>
2018-12-18 16:30:41 -0800

    lots of fixes to get install to work which included building blosc from source, and correctly settting selinux permissions for apache user to write files

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,10 @@
 
 # install base packages
 yum install -y epel-release git gzip tar
-yum install -y wget bzip2 bzip2-utils bzip2-devel gcc gcc-c++ hdf5 hdf5-devel httpd httpd-devel lzo lzo-devel blosc-devel blosc
+yum install -y wget bzip2 bzip2-utils bzip2-devel gcc gcc-c++ hdf5 hdf5-devel 
+yum install -y httpd httpd-devel 
+yum install -y lzo lzo-devel cmake
+yum install -y policycoreutils-python setroubleshoot
 
 # open port 5000 for http
 firewall-cmd --permanent --add-port=5000/tcp
@@ -30,34 +33,64 @@ echo "export PATH=/opt/miniconda3/bin:$PATH" >> /root/.bash_profile
 echo "export PATH=/opt/miniconda3/bin:$PATH" >> /root/.bashrc
 sudo -u vagrant echo "export PATH=/opt/miniconda3/bin:$PATH" >> /home/vagrant/.bash_profile
 
+wget https://github.com/Blosc/c-blosc/archive/1.15.1.tar.gz
+tar -zxf 1.15.1.tar.gz
+rm -f 1.15.1.tar.gz
+pushd c-blosc-1.15.1
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+cmake --build . --target install
+popd
+rm -rf c-blosc-1.15.1
+
 conda install -y -c conda-forge python-igraph
 conda install -y -c anaconda flask
 conda install -y -c conda-forge flask-restplus 
 
+# install mod_wsgi for apache
 pip install mod_wsgi
 
-git clone https://github.com/ndexbio/ndex2-client.git
+# install ndex2-client
+git clone -b 'chrisdev' --single-branch --depth 1 https://github.com/ndexbio/ndex2-client.git
 pushd ndex2-client
-git checkout chrisdev
 python setup.py bdist_wheel
 pip install dist/ndex2-*whl
 popd
+rm -rf ndex2-client
 
-git clone https://github.com/shfong/nbgwas.git
+# install nbgwas
+git clone -b 'v0.4.1.chrisdev' --single-branch --depth 1 https://github.com/shfong/nbgwas.git
 pushd nbgwas
-git checkout api
-rm -rf app
 python setup.py build
 python setup.py install
 popd
 
-git clone https://github.com/idekerlab/nbgwas_rest.git
+# install nbgwas_rest
+# TODO this should install the version in /vagrant
+git clone -b 'chrisdev' --single-branch --depth 1 https://github.com/idekerlab/nbgwas_rest.git
 pushd nbgwas_rest
-git checkout taskbased
 make dist
 pip install dist/nbgwas*whl
-cp nbgwas.httpconf /etc/httpd/conf.d/nbgwas.conf
 popd
+
+# install latest nodejs and npm
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+yum install -y nodejs
+
+# install NBGWAS-Frontend
+git clone -b 'gh-pages' --single-branch --depth 1 https://github.com/BrettJSettle/NBGWAS-Frontend.git
+pushd NBGWAS-Frontend
+npm install .
+cat package.json | sed "s/\"homepage\":.*/\"homepage\": \"http:\/\/localhost\"/" > package.tmp
+mv -f package.tmp package.json
+npm run build
+cp -a build/* /var/www/html/.
+popd
+
+
+# copy the http configuration file
+cp nbgwas.httpconf /etc/httpd/conf.d/nbgwas.conf
 
 mkdir /var/www/nbgwas_rest
 
@@ -78,7 +111,6 @@ WAIT_COUNT=600
 SLEEP_TIME=1
 EOF
 
-
 mkdir -p /var/www/nbgwas_rest/tasks/submitted
 mkdir -p /var/www/nbgwas_rest/tasks/processing
 mkdir -p /var/www/nbgwas_rest/tasks/done
@@ -88,7 +120,12 @@ chown -R apache.apache /var/www/nbgwas_rest/tasks
 
 mod_wsgi-express module-config > /etc/httpd/conf.modules.d/02-wsgi.conf
 
+# https://www.serverlab.ca/tutorials/linux/web-servers-linux/configuring-selinux-policies-for-apache-web-servers/
 # and tell SElinux its okay if apache writes to the directory
+semanage fcontext -a -t httpd_sys_rw_content_t "/var/www/nbgwas_rest/tasks(/.*)?"
+restorecon -Rv /var/www/nbgwas_rest/tasks
 
-chcon -R -t httpd_sys_rw_content_t /var/www/nbgwas_rest/tasks
+service httpd start
+
+echo "Visit http://localhost:8081/rest/v1 in your browser"
 
